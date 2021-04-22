02d876a9df3b5ab3f597ba24c340a09a3fe0c45f
chant <pengchuan@soyoung.com>
2021-04-22 11:47:19 +0800

    刷题

--- leet/其他/[35]搜索插入位置.py
+++ leet/其他/[35]搜索插入位置.py
@@ -35,6 +35,30 @@ from typing import List
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        # loc = len(nums)
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] >= target:
+                # loc = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
+
+    def searchInsert1(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)
+        while l < r:
+            mid = (l + r) >> 1
+            if nums[mid] == target:
+                r = mid
+            elif nums[mid] > target:
+                r = mid
+            elif nums[mid] < target:
+                l = mid + 1
+        return l
+
+    def searchInsert0(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums)
         while l < r:
             mid = (l + r) >> 1
