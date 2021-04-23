19d8b419532c40335fe2df68de796a7a15103814
chant <pengchuan@soyoung.com>
2021-04-23 15:22:37 +0800

    刷题

---
+++ leet/动态规划/背包问题/[322]零钱兑换.py
@@ -0,0 +1,76 @@
+# 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回
+#  -1。 
+# 
+#  你可以认为每种硬币的数量是无限的。 
+# 
+#  
+# 
+#  示例 1： 
+# 
+#  
+# 输入：coins = [1, 2, 5], amount = 11
+# 输出：3 
+# 解释：11 = 5 + 5 + 1 
+# 
+#  示例 2： 
+# 
+#  
+# 输入：coins = [2], amount = 3
+# 输出：-1 
+# 
+#  示例 3： 
+# 
+#  
+# 输入：coins = [1], amount = 0
+# 输出：0
+#  
+# 
+#  示例 4： 
+# 
+#  
+# 输入：coins = [1], amount = 1
+# 输出：1
+#  
+# 
+#  示例 5： 
+# 
+#  
+# 输入：coins = [1], amount = 2
+# 输出：2
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  1 <= coins.length <= 12 
+#  1 <= coins[i] <= 231 - 1 
+#  0 <= amount <= 104 
+#  
+#  Related Topics 动态规划 
+#  👍 1224 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        """完全背包问题，正向遍历"""
+        dp = [0] + [amount + 2] * amount
+
+        for coin in coins:
+            # 完全背包问题，必须正向遍历，因为不应该转移到dp[i−1][j−w[i]]而应该转移到dp[i][j−w[i]]，所以需要覆盖
+            for x in range(coin, amount + 1):
+                dp[x] = min(dp[x], dp[x - coin] + 1)
+        return dp[amount] if dp[amount] != amount + 2 else -1
+
+    def coinChange1(self, coins: List[int], amount: int) -> int:
+        dp = [0] + [amount + 2] * amount
+        for i in range(len(coins)):
+            for j in range(coins[i], amount + 1):
+                dp[j] = min(dp[j], dp[j - coins[i]] + 1)
+        return dp[amount] if dp[amount] != amount + 2 else -1
+# leetcode submit region end(Prohibit modification and deletion)
