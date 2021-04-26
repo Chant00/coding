cd9fb7a9cf419bb3e4ce581f6f1fed6197e82cde
chant <pengchuan@soyoung.com>
2021-04-26 20:56:14 +0800

    动态规划

--- leet/贪心算法/[455]分发饼干.py
+++ leet/贪心算法/[455]分发饼干.py
@@ -45,7 +45,11 @@ from typing import List
 
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        """排序+贪心"""
+        """排序+贪心
+        因为饥饿度最小的孩子最容易吃饱，所以我们先考虑这个孩子。为了尽量使得剩下的饼干可 以满足饥饿度更大的孩子，
+        所以我们应该把大于等于这个孩子饥饿度的、且大小最小的饼干给这 个孩子。
+        满足了这个孩子之后，我们采取同样的策略，考虑剩下孩子里饥饿度最小的孩子，直到 没有满足条件的饼干存在。
+        """
         g.sort()
         s.sort()
         child, cookie = 0, 0
