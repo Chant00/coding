3e2cbbbef93588e0af36213b2134fcf306a31322
chant <pengchuan@soyoung.com>
2021-04-27 16:17:47 +0800

    动态规划

---
+++ leet/贪心算法/[665]非递减数列.py
@@ -0,0 +1,77 @@
+# 给你一个长度为 n 的整数数组，请你判断在 最多 改变 1 个元素的情况下，该数组能否变成一个非递减数列。 
+# 
+#  我们是这样定义一个非递减数列的： 对于数组中任意的 i (0 <= i <= n-2)，总满足 nums[i] <= nums[i + 1]。 
+# 
+#  
+# 
+#  示例 1: 
+# 
+#  
+# 输入: nums = [4,2,3]
+# 输出: true
+# 解释: 你可以通过把第一个4变成1来使得它成为一个非递减数列。
+#  
+# 
+#  示例 2: 
+# 
+#  
+# 输入: nums = [4,2,1]
+# 输出: false
+# 解释: 你不能在只改变一个元素的情况下将其变为非递减数列。
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  1 <= n <= 10 ^ 4 
+#  - 10 ^ 5 <= nums[i] <= 10 ^ 5 
+#  
+#  Related Topics 数组 
+#  👍 571 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+from typing import List
+
+
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        """简化代码"""
+        flag = True
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:  # 要么缩小nums[i]，要么放大nums[i+1]
+                if flag:  # 如果还有修改机会，进行修改
+                    # 注意i == 0的特殊情况
+                    if i == 0 or nums[i + 1] >= nums[i - 1]:
+                        nums[i] = nums[i + 1]  # 缩小nums[i]，因为nums[i+1]比nums[i-1]大
+                    else:
+                        nums[i + 1] = nums[i]  # 放大nums[i+1]，因为nums[i+1]太小了比nums[i-1]还小
+                    flag = False  # 用掉唯一的修改机会
+                else:  # 没有修改机会，直接结束
+                    return False
+        return True
+
+    def checkPossibility1(self, nums: List[int]) -> bool:
+        """
+        遍历数组，如果遇到递减：
+            还能修改：
+                修改方案1：将nums[i]缩小至nums[i + 1]；
+                修改方案2：将nums[i + 1]放大至nums[i]；
+            不能修改了：直接返回false；
+        """
+        if len(nums) == 1: return True
+        flag = nums[0] <= nums[1]  # 标识是否还能修改
+        for i in range(1, len(nums) - 1):
+            if nums[i] > nums[i + 1]:  # 要么缩小nums[i]，要么放大nums[i+1]
+                if flag:  # 如果还有修改机会，进行修改
+                    if nums[i + 1] >= nums[i - 1]:
+                        nums[i] = nums[i + 1]  # 缩小nums[i]，因为nums[i+1]比nums[i-1]大
+                    else:
+                        nums[i + 1] = nums[i]  # 放大nums[i+1]，因为nums[i+1]太小了比nums[i-1]还小
+                    flag = False  # 用掉唯一的修改机会
+                else:  # 没有修改机会，直接结束
+                    return False
+        return True
+# leetcode submit region end(Prohibit modification and deletion)
