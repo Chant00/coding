533b2671daef611a6e064c2d66b18d1187b53442
chant <pengchuan@soyoung.com>
2021-04-28 10:25:23 +0800

    动态规划

--- leet/双指针/滑动窗口/[76]最小覆盖子串.py
+++ leet/双指针/滑动窗口/[76]最小覆盖子串.py
@@ -35,18 +35,50 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        """重点是要理解，lookup中1是临界点"""
+        lookup = dict()
+        for ch in t:
+            lookup[ch] = lookup.get(ch, 0) + 1
+        # Q1: counter = len(lookup)，那么后面的判断是lookup[s[r]] == 0，如果是len(t)那么后面的判就得改为lookup[s[r]] >= 0
+        count = len(lookup)
+
+        l = r = 0
+        start, min_len = 0, len(s) + 1
+
+        while r < len(s):
+            if s[r] in lookup:
+                lookup[s[r]] -= 1
+                # 这里的判断重点是要理解lookup中的值代表什么，1是一个临界点
+                if lookup[s[r]] == 0:
+                    count -= 1
+            r += 1
+
+            while count == 0:
+                if s[l] in lookup:
+                    lookup[s[l]] += 1
+                    if lookup[s[l]] == 1:
+                        count += 1
+                        # Q2: 为什么min_len的更新不能放在while循环外？因为求的是最小值，left在移动过程中窗口长度会越来越小，但是这些窗口是不满足条件的。
+                        if r - l < min_len:  # 这里总是写错成>
+                            start, min_len = l, r - l
+                l += 1
+        return s[start:start + min_len] if min_len < len(s) + 1 else ''
+
+    def minWindow1(self, s: str, t: str) -> str:
+        """重点是要理解，lookup中1是临界点"""
         lookup = dict()
         for i in t:
             lookup[i] = lookup.get(i, 0) + 1
 
         left, right = 0, 0
-        # Q1: 为什么不是 counter = len(lookup)？
+        # Q1: counter = len(lookup)，那么后面的判断是lookup[s[right]] == 1，如果是len(t)那么后面的判就得改为lookup[s[r]] >= 1
         counter = len(lookup)
         start = 0
         min_len = len(s) + 1
 
         while right < len(s):
             if s[right] in lookup:
+                # 这里的判断重点是要理解lookup中的值代表什么，1是一个临界点
                 if lookup[s[right]] == 1:
                     counter -= 1
                 lookup[s[right]] -= 1
@@ -57,7 +89,7 @@ class Solution:
 
                     if lookup[s[left]] == 0:
                         counter += 1
-                        # Q2: 为什么min_len的更新不能放在while循环外？
+                        # Q2: 为什么min_len的更新不能放在while循环外？因为求的是最小值，left在移动过程中窗口长度会越来越小，但是这些窗口是不满足条件的。
                         if min_len > right - left:
                             min_len = right - left
                             start = left
