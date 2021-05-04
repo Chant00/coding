24b64777f3859c6d5b35a3399d2b7895024af825
chant <pengchuan@soyoung.com>
2021-05-04 13:16:58 +0800

    排序

--- leet/排序/test.py
+++ leet/排序/test.py
@@ -8,48 +8,41 @@ Created on 5/2/21
 import random
 
 
-def quick_sort1(arr, low, high):
-    """常见的双指针双向奔赴快排"""
-    if low >= high:
-        return arr
-    # 随机选取基准数
-    rand = random.randint(low, high)
-    arr[rand], arr[low] = arr[low], arr[rand]
-    base = arr[low]
-    l, r = low, high
-    while l < r:
-        # 下面两个while的顺序不能变，必须让right先走
-        while arr[r] > base and l < r:
-            r -= 1
-        arr[l] = arr[r]
-
-        while arr[l] <= base and l < r:  # 注意<=因为上面是arr[r] > base
-            l += 1
-        arr[r] = arr[l]
-    arr[l] = base
-    quick_sort1(arr, low, l - 1)
-    quick_sort1(arr, l + 1, high)
+def counting_sort(arr, max_value):
+    bucket_size = max_value + 1
+    bucket = [0] * bucket_size
+    for i in arr:
+        bucket[i] += 1
+    idx = 0
+    for i in range(bucket_size):
+        for _ in range(bucket[i]):
+            arr[idx] = i
+            idx += 1
     return arr
 
 
-def partition(arr, low, high):
-    rand = random.randint(low, high)
-    arr[rand], arr[high] = arr[high], arr[rand]
-    i = low
-    for j in range(low, high):
-        if arr[j] < arr[high]:
-            arr[i], arr[j] = arr[j], arr[i]
-            i += 1
-    arr[i], arr[high] = arr[high], arr[i]
-    return i
+def sift_down(arr, start, end):
+    parent, child = start, 2 * start + 1
+    while child < end:
+        if child + 1 < end and arr[child] < arr[child + 1]:
+            child += 1
+        if arr[parent] < arr[child]:
+            arr[parent], arr[child] = arr[child], arr[parent]
+            parent = child
+            child = 2 * parent + 1
+        else:
+            break
+    return arr
 
 
-def quick_sort(arr, low, high):
-    if low >= high:
-        return arr
-    i = partition(arr, low, high)
-    quick_sort(arr, low, i - 1)
-    quick_sort(arr, i + 1, high)
+def heap_sort(arr):
+    # 创建大顶堆
+    for start in range((len(arr) - 2) // 2, -1, -1):
+        sift_down(arr, start, len(arr))
+    # 堆排序
+    for end in range(len(arr) - 1, 0, -1):
+        arr[0], arr[end] = arr[end], arr[0]
+        sift_down(arr, 0, end)
     return arr
 
 
@@ -57,22 +50,27 @@ def test():
     import numpy as np
     from copy import deepcopy
 
-    a = np.random.randint(0, 1000, size=10)
+    max_value = 100
+    a = np.random.randint(0, max_value, size=20)
 
-    quick_sort(deepcopy(a), 0, len(a) - 1)
-    quick_sort1(deepcopy(a), 0, len(a) - 1)
+    counting_sort(list(a), max_value)
+    heap_sort(list(a))
+    np.sort(a[0:])
 
     for _ in range(30):
-        a = np.random.randint(0, 1000, size=1000)
-        a = sorted(a, reverse=True)
-        a = sorted(a)
+        max_value = 1000
+        a = np.random.randint(0, max_value, size=1000)
+        # a = sorted(a, reverse=True)
+        # a = sorted(a)
         # assert all(np.sort(a[1:]) == bubble_sort(a[1:]))
         # assert all(np.sort(a[1:]) == select_sort(a[1:]))
         # assert all(np.sort(a[1:]) == insertion_sort(a[1:]))
         # assert all(np.sort(a[1:]) == shell_sort(a[1:]))
         # assert all(np.sort(a[0:]) == merge_sort(a[0:]))
+        assert all(np.sort(a[0:]) == heap_sort(a[0:]))
+        assert all(np.sort(a[0:]) == counting_sort(a[0:], max_value))
         # assert all(quick_sort1(deepcopy(a), 0, len(a) - 1) == np.sort(deepcopy(a)))
-        assert all(quick_sort(deepcopy(a), 0, len(a) - 1) == np.sort(deepcopy(a)))
+        # assert all(quick_sort(deepcopy(a), 0, len(a) - 1) == np.sort(deepcopy(a)))
     """
     b = list(a)
     %timeit merge_sort(b[0:])
