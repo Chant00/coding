65c91140003f21775d4c8e65f62236ea5996042c
chant <pengchuan@soyoung.com>
2021-05-06 11:09:01 +0800

    排序

---
+++ leet/排序/[75]颜色分类.py
@@ -0,0 +1,96 @@
+# 给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。 
+# 
+#  此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。 
+# 
+#  
+#  
+# 
+#  
+# 
+#  示例 1： 
+# 
+#  
+# 输入：nums = [2,0,2,1,1,0]
+# 输出：[0,0,1,1,2,2]
+#  
+# 
+#  示例 2： 
+# 
+#  
+# 输入：nums = [2,0,1]
+# 输出：[0,1,2]
+#  
+# 
+#  示例 3： 
+# 
+#  
+# 输入：nums = [0]
+# 输出：[0]
+#  
+# 
+#  示例 4： 
+# 
+#  
+# 输入：nums = [1]
+# 输出：[1]
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  n == nums.length 
+#  1 <= n <= 300 
+#  nums[i] 为 0、1 或 2 
+#  
+# 
+#  
+# 
+#  进阶： 
+# 
+#  
+#  你可以不使用代码库中的排序函数来解决这道题吗？ 
+#  你能想出一个仅使用常数空间的一趟扫描算法吗？ 
+#  
+#  Related Topics 排序 数组 双指针 
+#  👍 874 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+from typing import List
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """三路快排，直接分为< = >三个部分"""
+        p0, p2 = 0, len(nums) - 1
+        i = 0
+        while i <= p2:
+            while i <= p2 and nums[i] == 2:
+                nums[i], nums[p2] = nums[p2], nums[i]
+                p2 -= 1
+            if nums[i] == 0:
+                nums[i], nums[p0] = nums[p0], nums[i]
+                p0 += 1
+            i += 1
+
+    def sortColors1(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        p0 = p1 = 0
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                nums[i], nums[p1] = nums[p1], nums[i]
+                p1 += 1
+            elif nums[i] == 0:
+                nums[i], nums[p0] = nums[p0], nums[i]
+                if p0 < p1:
+                    nums[i], nums[p1] = nums[p1], nums[i]
+                p0 += 1
+                p1 += 1
+
+
+# leetcode submit region end(Prohibit modification and deletion)
+Solution().sortColors([2, 0, 2, 1, 1, 0])
--- leet/排序/test.py
+++ leet/排序/test.py
@@ -9,45 +9,25 @@ import random
 import numpy as np
 
 
-def counting_sort(arr, max_value):
-    bucket_size = max_value + 1
-    bucket = [0] * bucket_size
-    for i in arr:
-        bucket[i] += 1
-    idx = 0
-    for i in range(bucket_size):
-        for _ in range(bucket[i]):
-            arr[idx] = i
-            idx += 1
-    return arr
+def _partition3(arr, low, high):
+    """三路快排，将数组分为三个部分, < = >。正常都是分为<= >两部分。可用于解决荷兰旗问题
+    todo: 这个写法有问题，待修正
+    """
+    p1, p2 = low, high
+    i = low
+    base = arr[low]
+    while i <= p2:
+        while i <= p2 and arr[i] > base:
+            arr[i], arr[p2] = arr[p2], arr[i]
+            p2 -= 1
+        if arr[i] < base:
+            arr[i], arr[p1] = arr[p1], arr[i]
+            p1 += 1
+        i += 1
+    return p1, p2 + 1
 
 
-def sift_down(arr, start, end):
-    parent, child = start, 2 * start + 1
-    while child < end:
-        if child + 1 < end and arr[child] < arr[child + 1]:
-            child += 1
-        if arr[parent] < arr[child]:
-            arr[parent], arr[child] = arr[child], arr[parent]
-            parent = child
-            child = 2 * parent + 1
-        else:
-            break
-    return arr
-
-
-def heap_sort(arr):
-    # 创建大顶堆
-    for start in range((len(arr) - 2) // 2, -1, -1):
-        sift_down(arr, start, len(arr))
-    # 堆排序
-    for end in range(len(arr) - 1, 0, -1):
-        arr[0], arr[end] = arr[end], arr[0]
-        sift_down(arr, 0, end)
-    return arr
-
-
-def _partition3(nums, left, right):
+def partition3(nums, left, right):
     p = nums[left]
     lt = left  # nums[left+1...lt] < base
     gt = right + 1  # nums[gt...right] > base
@@ -64,65 +44,28 @@ def _partition3(nums, left, right):
         else:
             gt -= 1
             nums[i], nums[gt] = nums[gt], nums[i]
-    print(nums)
-    print(f'i={i}, lt:{lt}, gt:{gt}')
     # 想清楚，为什么交换 left 和 lt
     nums[left], nums[lt] = nums[lt], nums[left]
-    print(nums)
     return lt, gt
 
 
-def partition3(arr, low, high):
-    """三路快排，将数组分为三个部分, < = >。正常都是分为<= >两部分。可用于解决荷兰旗问题"""
-    p1, p2 = low, high
-    i = 0
-    base = arr[low]
-    print(base)
-    while i <= p2 and p1 < p2:
-        while i <= p2 and arr[i] > base:
-            print(f'i={i}, p1:{p1}, p2:{p2}')
-            print(arr)
-            arr[i], arr[p2] = arr[p2], arr[i]
-            p2 -= 1
-
-        if arr[i] < base:
-            print(f'i={i}, p1:{p1}, p2:{p2}')
-            print(arr)
-            arr[i], arr[p1] = arr[p1], arr[i]
-            p1 += 1
-        i += 1
-
-    print(f'i={i}, p1:{p1}, p2:{p2}')
-    print(arr)
-    return p1, p2 + 1
-
-
-def quick_sort(arr, low, high):
+def quick_sort3(arr, low, high):
     """快速排序"""
-    if low < high:  # 这个判断一定要写，否则就会无限递归，超出递归深度，这是递归的终止条件
-        p1, p2 = partition3(arr, low, high)
-        quick_sort(arr, low, p1 - 1)
-        quick_sort(arr, p2, high)
+    if low < high:  # 这个判断和最后的return一定要写，否则就会无限递归，超出递归深度，这是递归的终止条件
+        p1, p2 = _partition3(arr, low, high)
+        quick_sort3(arr, low, p1 - 1)
+        quick_sort3(arr, p2, high)
     return arr
-
-
-a = [90, 3, 6, 2, 8, 90, 11, 87, 91, 94]
-_partition3(list(a), 0, len(a) - 1)
-a = [3, 6, 2, 8, 11, 87, 90, 90, 94, 91]
-p1, p2 = partition3(a, 0, 5)
-partition3(a, p2, len(a) - 1)
-
-quick_sort(list(a), 0, len(a) - 1)
 
 
 def test():
     max_value = 100
     a = np.random.randint(0, max_value, size=10)
-    quick_sort(list(a), 0, len(a) - 1)
+    quick_sort3(list(a), 0, len(a) - 1)
 
     for _ in range(30):
         a = np.random.randint(0, 1000, size=100)
-        assert quick_sort(list(a), 0, len(a) - 1) == sorted(list(a))
+        assert quick_sort3(list(a), 0, len(a) - 1) == sorted(list(a))
 
     """
     b = list(a)
