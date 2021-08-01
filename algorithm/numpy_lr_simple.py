8d0cd57f013441a06cf9e031bb63119ff343a41e
pengchuan <pengchuan@soyoung.com>
2021-08-01 22:45:26 +0800

    lr

--- algorithm/numpy_lr_simple.py
+++ algorithm/numpy_lr_simple.py
@@ -50,6 +50,38 @@ def LR(X, y, lr=0.05, count=10):
     return l, w
 
 
+class MyLR:
+    def __init__(self, lr=0.05, max_ite=10):
+        self.lr = lr
+        self.max_ite = max_ite
+        self.w = None
+
+    def predict(self, X, threshold=0.6):
+        intercept = np.ones((X.shape[0], 1))
+        X = np.concatenate((intercept, X), axis=1)
+        h = sigmoid(np.dot(X, self.w))
+        if threshold is None:
+            return h
+        return np.array([1 if i > threshold else 0 for i in h])
+
+    def fit(self, X, y):
+        # 补上截距项
+        intercept = np.ones((X.shape[0], 1))
+        X = np.concatenate((intercept, X), axis=1)
+
+        self.w = np.zeros(X.shape[1])  # 初始化参数为0
+
+        for i in range(self.max_ite):
+            h = sigmoid(np.dot(X, self.w))
+            g = gradient(X, h, y)
+            self.w -= self.lr * g
+            print(f'epoch = {i}, loss = {loss(h, y)}')
+
+    def score(self, X, y):
+        from sklearn.metrics import accuracy_score
+        return accuracy_score(y, self.predict(X))
+
+
 def sk_lr():
     """
     调用sklearn LR，训练模型，打印准确率
@@ -58,8 +90,17 @@ def sk_lr():
     data = datasets.load_iris()
     X = data['data'][50:150]
     y = data['target'][50:150]
+    # 由于y里面是1和2，所以清洗一下标签
+    y = np.array([i - 1 for i in y])
     lr = LogisticRegression()
     lr.fit(X, y)
     print('sklear-lr:', lr.score(X[0:100], y[0:100]))
 
-    l, w = LR(X, y)
+    # l, w = LR(X, y)
+    my_lr = MyLR(max_ite=300)
+    my_lr.fit(X, y)
+    my_lr.predict(X)
+    print('numpy-lr:', my_lr.score(X, y))
+
+
+sk_lr()
