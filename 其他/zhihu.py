b9ed956cec3c64184e2cde7de4766cfdf196c436
chant <pengchuan@soyoung.com>
2021-08-16 10:52:07 +0800

    排序

--- 其他/zhihu.py
+++ 其他/zhihu.py
@@ -30,3 +30,46 @@ def remove_sub(alpha, beta):
 
 print(remove_sub('ababccabcaaa', 'abc'))
 print(remove_sub('abaabcaaabcaaa', 'abc'))
+
+"""
+计算完全二叉树的节点数量
+叶子节点的高度差不超过一
+最后一层最左边连续排列
+[1,2,3,4,5]
+2*n+1 2*n+2
+root
+m代表层数
+1
+2
+2**(m-1)
+2**m-1=n
+m = lg(n-1)
+
+2**(lg(n-1)-1)
+n
+"""
+
+class Node:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+def num_of_tree(root):
+    cnt = 1
+    layer1 = [root]
+    layer2 = []
+    while True:
+        for i in layer1:
+            if i.left:
+                layer2.append(i.left)
+            if i.right:
+                layer2.append(i.right)
+
+        cnt += len(layer2)
+        if layer2[0].left is None and layer2[0].right is None:
+            break
+        layer1 = layer2
+        layer2 = []
+
+    return cnt
