a50a5e8b8524ba808f98621b1041ec94adb4bf62
pengchuan.666 <pengchuan.666@bytedance.com>
2024-08-20 17:18:35 +0800

    中位数

--- leet/二分查找/[69]x 的平方根.py
+++ leet/二分查找/[69]x 的平方根.py
@@ -26,7 +26,7 @@ import math
 
 
 class Solution:
-    def mySqrt(self, x: int) -> int:
+    def mySqrt0(self, x: int) -> int:
         """袖珍计算器算法，用指数函数 exp  和对数函数 ln  代替平方根函数的方法
         x^0.5 = (e^ln(x))^0.5 = e^(0.5*ln(x))"""
         if x == 0:
@@ -35,21 +35,53 @@ class Solution:
         # 浮点数问题，我们应当找出 ans  与 ans1  中哪一个是真正的答案
         return ans + 1 if (ans + 1) ** 2 <= x else ans
 
-    def mySqrt3(self, x: int) -> int:
+    def mySqrt(self, x: int) -> int:
+        """牛顿法
+        这里先把x改名为n，方便写函数推导，写代码的时候再反过来。
+        定义函数 f(x) = x**2 - n
+        一阶泰勒展开，并令其=0求驻点 f(x0) + f'(x0)(x-x0) = 0
+        x = x0-f(x0)/f'(x0)
+          = x0- (x0-n/x0)/2
+        """
+        if x == 0:
+            return 0
+        threshold = 0.0001
+        n = x
+        while True:
+            pre = n
+            n = n - (n - x / n) / 2
+            # 这里比检查abs(mid ** 2 - x) <= threshold更好，一个是不用再计算平方，
+            # 一个是可能出现死循环，threshold=1e-7，x=2147395599时，46339.999989210184会一直死循环，只能是调小阈值
+            if abs(n - pre) < threshold:
+                break
+        return int(n)
+
+    def mySqrt3_1(self, x: int) -> int:
         """牛顿法"""
         if x == 0:
             return 0
+        threshold = 0.0001
+        n = x
+        pre = n
+        n = n - (n - x / n) / 2
+        while abs(n - pre) > threshold:
+            pre = n
+            n = n - (n - x / n) / 2
+        return int(n)
 
-        C, x0 = float(x), float(x)
-        while True:
-            xi = 0.5 * (x0 + C / x0)
-            # 这里比检查abs(mid ** 2 - x) <= threshold更好，一个是不用再计算平方，
-            # 一个是可能出现死循环，threshold=1e-7，x=2147395599时，46339.999989210184会一直死循环，只能是调小阈值
-            if abs(x0 - xi) < 1e-7:
-                break
-            x0 = xi
-
-        return int(x0)
+    def mySqrt4(self, x: int) -> int:
+        """牛顿法
+        判断条件abs(n ** 2 - x) > threshold，计算平方，不如mySqrt3
+        f(x) = x**2 - n
+        f(x0) + f'(x0)(x-x0) = 0
+        x = x0-f(x0)/f'(x0)
+          = x0- (x0-n/x0)/2
+        """
+        threshold = 0.001
+        n = x
+        while abs(n ** 2 - x) > threshold:
+            n = n - (n - x / n) / 2
+        return int(n)
 
     def mySqrt1(self, x: int) -> int:
         """二分法1
