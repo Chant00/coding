56e7b14f5c44a98e8789a1a286322377329316bc
pengchuan.666 <pengchuan.666@bytedance.com>
2024-08-25 17:05:57 +0800

    鸡蛋掉落问题

--- leet/动态规划/[887]鸡蛋掉落.py
+++ leet/动态规划/[887]鸡蛋掉落.py
@@ -1,4 +1,3 @@
-
 # 给你 k 枚相同的鸡蛋，并可以使用一栋从第 1 层到第 n 层共有 n 层楼的建筑。
 # 
 #  已知存在楼层 f ，满足 0 <= f <= n ，任何从 高于 f 的楼层落下的鸡蛋都会碎，从 f 楼层或比它低的楼层落下的鸡蛋都不会破。 
@@ -49,12 +48,47 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def superEggDrop(self, k: int, n: int) -> int:
-        """李永乐老师讲解写法，逻辑没问题，但是superEggDrop(4, 5000))会超时，todo:需要用二分法优化，官方题解的方法1
+        """李永乐老师讲解写法，逻辑没问题，但是superEggDrop(4, 5000))会超时，所以这里叠加二分法优化
+        官方题解的方法1相同，只是官解用的带记忆的递归，这里用的循环。
+        复杂度O(kn2) 降低至 O(knlogn)
         Mi(n,k) = max(M(i,k-1), M(n-i,k)) + 1
         M(n,k) = min(M1,M2,...Mn)
         """
-        # if n >= 5000:
-        #     return 5000
+        #  dp[i][j]表示共i层楼j个鸡蛋时最少要试几次保证能找到临界点
+        dp = [[0] * (k + 1) for _ in range(n + 1)]
+        for i in range(1, n + 1):
+            # 鸡蛋数量为1时，只能一层层从低到高去试，所以共i次
+            dp[i][1] = i
+            for j in range(2, k + 1):
+                # dp[x-1][j-1]随着h递增，dp[i - x][j]随着x递减，min(max())其实两个函数的就是交点
+                # 二分查找，dp[mid-1][j-1] > dp[i - mid][j] 则要找的那个x'就在mid右边，否则在mid左边
+                # 最后比较交点左右两侧的两个值就可以，不用遍历计算全部
+                l, r = 1, i
+                while l + 1 < r:
+                    mid = l + (r - l) // 2
+                    t1 = dp[mid - 1][j - 1]
+                    t2 = dp[i - mid][j]
+                    if t1 < t2:
+                        l = mid
+                    elif t1 > t2:
+                        r = mid
+                    elif t1 == t2:
+                        l = r = mid
+
+                dp[i][j] = min(max(dp[t - 1][j - 1], dp[i - t][j]) + 1 for t in (l, r))
+                # 原本要遍历i次全部计算，二分法使得这一层的复杂度从O(n)降低为O(log(n))
+                # dp[i][j] = n  # 最大也就n次
+                # for x in range(1, i + 1):
+                #     Mh = max(dp[x - 1][j - 1], dp[i - x][j]) + 1
+                #     dp[i][j] = min(dp[i][j], Mh)
+        return dp[n][k]
+
+    def superEggDrop0(self, k: int, n: int) -> int:
+        """李永乐老师讲解写法，逻辑没问题，但是superEggDrop(4, 5000))会超时，需要用二分法优化，官方题解的方法1
+        复杂度O(kn2)
+        Mi(n,k) = max(M(i,k-1), M(n-i,k)) + 1
+        M(n,k) = min(M1,M2,...Mn)
+        """
         #  dp[i][j]表示共i层楼j个鸡蛋时最少要试几次保证能找到临界点
         dp = [[0] * (k + 1) for _ in range(n + 1)]
         for i in range(1, n + 1):
@@ -65,14 +99,23 @@ class Solution:
                 for h in range(1, i + 1):
                     Mh = max(dp[h - 1][j - 1], dp[i - h][j]) + 1
                     dp[i][j] = min(dp[i][j], Mh)
-                if dp[i][k] >= n:
-                    dp[i][k] = i
-                    break
         # return dp[i][j]
         return dp[n][k]
 
     def superEggDrop2(self, k: int, n: int) -> int:
-        """官方题解方法3：数学法"""
+        """官方题解方法3：数学法。时间复杂度：O(kn)
+        dp[i][j] 不存储直接的答案，转换思路，存一个中间变量，同时能写出状态转移方程就行。
+
+        反过来想这个问题：如果我们可以做 t 次操作，而且有 k 个鸡蛋，那么我们能找到答案的最高的 n 是多少？
+        我们设 f(t,k) 为在上述条件下的 n。如果我们求出了所有的 f(t,k)，那么只需要找出最小的满足 f(t,k)≥n 的 t。
+
+        那么我们如何求出 f(t,k) 呢？我们还是使用动态规划。因为我们需要找出最高的 n，因此我们不必思考到底在哪里扔这个鸡蛋，
+        我们只需要扔出一个鸡蛋，看看到底发生了什么：
+            如果鸡蛋没有碎，那么对应的是 f(t−1,k)，也就是说在这一层的上方可以有 f(t−1,k) 层；
+            如果鸡蛋碎了，那么对应的是 f(t−1,k−1)，也就是说在这一层的下方可以有 f(t−1，k−1) 层。
+            状态转移方程：f(t,k)=1+f(t−1,k−1)+f(t−1,k)
+            边界条件为：当 t≥1 的时候 f(t,1)=t，当 k≥1 时，f(1,k)=1。
+        """
         if n == 1:
             return 1
         f = [[0] * (k + 1) for _ in range(n + 1)]
@@ -88,10 +131,9 @@ class Solution:
         return ans
 
 
-print(Solution().superEggDrop(2, 6))  # 3
-print(Solution().superEggDrop(1, 2))  # 2
-print(Solution().superEggDrop(3, 14))  # 4
-print(Solution().superEggDrop(2, 3))  # 2
-print(Solution().superEggDrop(4, 5000))  # 19
-print(Solution().superEggDrop2(4, 5000))  # 19
+# k1 = 3
+# n1 = 39
+# print(Solution().superEggDrop(k1, n1))  # 19
+# print(Solution().superEggDrop0(k1, n1))  # 19
+# print(Solution().superEggDrop2(k1, n1))  # 19
 # leetcode submit region end(Prohibit modification and deletion)
